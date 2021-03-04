fec0badec66f078992963719e485098e3dfcfa82
loet <ueli.loetscher@gmail.com>
2021-03-04 17:00:35 +0100

    moved Transaction Annotations

--- src/main/java/ch/mobi/ueliloetscher/learning/employeemanagement/boundary/EmployeeManagementResource.java
+++ src/main/java/ch/mobi/ueliloetscher/learning/employeemanagement/boundary/EmployeeManagementResource.java
@@ -13,40 +13,33 @@ public interface EmployeeManagementResource {
     @GET()
     @Path("/ping")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.SUPPORTS)
     Response ping();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.SUPPORTS)
     Response searchEmployees(@QueryParam("ename") String ename);
 
     @GET
     @Path("/{eid}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.SUPPORTS)
     Response getEmployee(@PathParam("eid") Long eid);
 
     @GET
     @Path("/slow/{eid}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.SUPPORTS)
     Response getEmployeeSlow(@PathParam("eid") Long eid);
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.REQUIRED)
     Response addEmployee(Employee employee) throws ValidationException;
 
     @PUT
     @Path("/{eid}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.REQUIRED)
     Response updateEmployee(@PathParam("eid") Long eid, Employee employee);
 
     @DELETE
     @Path("/{eid}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional(Transactional.TxType.REQUIRED)
     Response deleteEmployee(@PathParam("eid") Long eid);
 }
